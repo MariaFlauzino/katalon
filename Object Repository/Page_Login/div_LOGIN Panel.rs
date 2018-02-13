387f093a4fd65bd4476c364852919405f487151e
Maria Flauzino <maria.flauzino@hotmail.com>
2018-02-13 21:28:57 -0200

    Incio projeto

---
+++ Object Repository/Page_Login/div_LOGIN Panel.rs
@@ -0,0 +1,59 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_LOGIN Panel</name>
+   <tag></tag>
+   <elementGuidId>7c7cd352-3977-46be-a73d-c68c8cfb07a1</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>id</name>
+      <type>Main</type>
+      <value>divLogin</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+    
+        
+    
+
+    
+        
+        
+                LOGIN Panel
+
+        
+                      Username 
+        
+        
+                     Password
+        
+        
+        
+            
+                    
+    
+
+</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;divLogin&quot;)</value>
+   </webElementProperties>
+</WebElementEntity>
